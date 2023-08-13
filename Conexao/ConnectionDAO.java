a2aa14355065ec2c9367a3bc27f15ec56d1317a2
Ytalo Mathues <101993839+ytalomath@users.noreply.github.com>
2023-08-13 16:33:19 -0300

    Add files via upload

---
+++ Conexao/ConnectionDAO.java
@@ -0,0 +1,70 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionDAO {
+    
+    //ATRIBUTOS PARA CONEX�O COM O BANCO DE DADOS, DRIVER, URL, USUARIO, SENHA
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://randomuser123.ddns.net:3306/radius?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "java";
+    private static final String PASS = "java";
+    
+    //METODO PARA ESTABELECER CONEX�O COM O BANCO DE DADOS
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Erro na conex�o: ", e);
+        }
+    }
+    
+    //METODO PARA ENCERRAR A CONEX�O COM O BANCO DE DADOS
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //METODO REDUDANTE PARA ENCERRAR A CONEX�O COM O BANCO DE DADOS
+    public static void closeConnection(Connection c, PreparedStatement stmt) {
+
+        closeConnection(c);
+
+        try {
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //METODO REDUNDANTE PARA ENCERRAR A CONEX�O COM O BANCO DE DADOS
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+
+        closeConnection(con, stmt);
+
+        try {
+
+            if (rs != null) {
+                rs.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
